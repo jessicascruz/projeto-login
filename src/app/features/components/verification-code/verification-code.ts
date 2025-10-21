36e5973fd894ff9f1f997e26c87eac29b6a99630
Jéssica Cruz <jessica.santos648@gmail.com>
2025-10-21 10:00:23 -0300

    Configuração e criação de componentes

---
+++ src/app/features/components/verification-code/verification-code.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-code',
+  imports: [],
+  templateUrl: './verification-code.html',
+  styleUrl: './verification-code.scss'
+})
+export class VerificationCode {
+
+}
